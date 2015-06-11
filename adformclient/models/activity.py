4d3d92532174e55601e1b5d9ec3bd693ef37562c
Emmanuel Job <emmanuel.job@accuenmedia.com>
2015-06-11 22:52:30 +0000

    add find_by_id

--- adformclient/models/activity.py
+++ adformclient/models/activity.py
@@ -19,3 +19,18 @@ class Activity(Base):
             rval.append(toAdd)
 
         return rval
+
+    def find_by_id(self, activity_id):
+        client = Client(Activity.get_wsdl_url())
+        header = {'Ticket': Activity.connection.get_authorization()}
+        client.set_options(soapheaders=header)
+        resp = client.service.GetActivities(ActivityId=activity_id)
+        rval = []
+        if len(resp[0]) != 1:
+            raise Exception("Should only find one obj for activity_id {0} found: {1}".format(str(activity_id), str(len(resp[0]))))
+        for obj in resp[0]:
+            toAdd = Activity(Activity.connection)
+            toAdd.update(obj)
+            return toAdd
+
+        return None
